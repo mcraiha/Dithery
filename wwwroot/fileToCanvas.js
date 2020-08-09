b5feed2b94ed3521f356f1b6bcec9312f4917922
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-08-09 15:46:20 +0300

    Remove unneeded Javascript
    No need for it

--- wwwroot/fileToCanvas.js
+++ wwwroot/fileToCanvas.js
@@ -1,49 +1,7 @@
-window.readWidthFromCanvas = (canvasId) => {
-  const canvas = document.getElementById(canvasId);
-  return canvas.width;
-}
-
-window.readHeightFromCanvas = (canvasId) => {
-  const canvas = document.getElementById(canvasId);
-  return canvas.height;
-}
-
-window.readPixelDataFromCanvas = (canvasId) => {
-  const canvas = document.getElementById(canvasId);
-  const ctx = canvas.getContext('2d');
-
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const returnArray = Array.prototype.slice.call(imageData.data);
-  return returnArray;
-}
 
 window.copyImageDataAsPng = (imgId) => {
   const imgProcessed = document.getElementById(imgId);
   return imgProcessed.src;
-}
-
-window.copyImageFromImgToAppImg = (imgId) => {
-  const imgProcessed = document.getElementById('canvasprocessed');
-  const imgCopyDestination = document.getElementById(imgId);
-  imgCopyDestination.src = imgProcessed.src;
-}
-
-window.writePixelDataToCanvas = (canvasId, width, height, bufferData) => {
-
-  console.log(width);
-  console.log(height);
-  console.log(bufferData.length);
-
-
-  const canvas = document.getElementById(canvasId);
-  canvas.width = width;
-  canvas.height = height;
-
-  const ctx = canvas.getContext('2d');
-  const clampedArray = Uint8ClampedArray.from(bufferData);
-
-  const imageData = new ImageData(clampedArray, width, height);
-  ctx.putImageData(imageData, 0, 0);
 }
 
 const imgInput = document.getElementById('imagetodither');
